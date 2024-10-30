475a59af0ce8f3594a53ef0991cfd0afb741ee26
Daniel Ospina <justeb9523@gmail.com>
2024-10-30 14:55:59 -0500

    Removed unused tests (not required), organized folder structure and naming.

---
+++ frontend/src/app/components/molecules/delete-prompt/delete-prompt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-prompt',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-prompt.component.html',
+  styleUrl: './delete-prompt.component.scss'
+})
+export class DeletePromptComponent {
+
+}
---
+++ frontend/src/app/components/molecules/image-picker/image-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-picker.component.html',
+  styleUrl: './image-picker.component.scss'
+})
+export class ImagePickerComponent {
+
+}
