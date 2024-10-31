1d550636d66c3359d16a5616cf11aa2f0c01a10d
Daniel Ospina <justeb9523@gmail.com>
2024-10-31 17:06:43 -0500

    Added env back since cloud needs billing

--- express-backend/routes/index.js
+++ express-backend/routes/index.js
@@ -3,9 +3,48 @@ const db = require("./public/javascripts/firebaseAdmin.js");
 
 const router = express.Router();
 
-router.get("/characters", async function (req, res, next) {});
-router.post("/characters", async function (req, res, next) {});
-router.delete("/characters:id", async function (req, res, next) {});
-router.post("/characters:id", async function (req, res, next) {});
+router.get("/characters/all", async (req, res) => {
+  try {
+    const charactersSnapshot = await db.collection("characters").get();
+    const characters = charactersSnapshot.docs.map((doc) => doc.data());
+    res.status(200).json(characters);
+  } catch (error) {
+    res.status(500).send("Error fetching characters: " + error.message);
+  }
+});
+
+router.post("/characters/create", async (req, res) => {
+  try {
+    const character = req.body;
+    await db
+      .collection("characters")
+      .doc(character.id.toString())
+      .set(character);
+    res.status(201).send("Character added successfully");
+  } catch (error) {
+    res.status(500).send("Error adding character: " + error.message);
+  }
+});
+
+router.delete("/characters/delete/:id", async (req, res) => {
+  try {
+    const characterId = req.params.id;
+    await db.collection("characters").doc(characterId).delete();
+    res.status(200).send("Character deleted successfully");
+  } catch (error) {
+    res.status(500).send("Error deleting character: " + error.message);
+  }
+});
+
+router.put("/characters/update/:id", async (req, res) => {
+  try {
+    const characterId = req.params.id;
+    const characterUpdates = req.body;
+    await db.collection("characters").doc(characterId).update(characterUpdates);
+    res.status(200).send("Character updated successfully");
+  } catch (error) {
+    res.status(500).send("Error updating character: " + error.message);
+  }
+});
 
 module.exports = router;
