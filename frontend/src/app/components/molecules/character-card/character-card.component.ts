c0f96ee9b4fe466a0f5f4e096f200c5f8d1a4e83
Daniel Ospina <justeb9523@gmail.com>
2024-10-30 22:42:06 -0500

    mobile tweaks, working on pagination

--- frontend/src/app/components/molecules/character-card/character-card.component.ts
+++ frontend/src/app/components/molecules/character-card/character-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Character } from '../../../models/character.model';
+import { CapitalizeFirstLetterPipe } from '../../../pipes/capitalize-first-letter.pipe';
 
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [],
+  imports: [CapitalizeFirstLetterPipe],
   templateUrl: './character-card.component.html',
   styleUrl: './character-card.component.scss',
 })
